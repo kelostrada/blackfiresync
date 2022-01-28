0000d0bf9845652213fcac258fcc056cabcae4e3
Bartosz Kalinowski <kelostrada@gmail.com>
2022-01-28 15:05:48 +0100

    add autoload

--- sync.php
+++ sync.php
@@ -4,6 +4,7 @@ define('_PS_MODE_DEV_', true);
 
 require_once __DIR__ . '/../../config/config.inc.php';
 require_once __DIR__ . '/../../init.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use Blackfire\BlackfireSyncService;
 
